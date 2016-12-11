c13d7e6de446762de00d14ce9da480fbad7aa60d
zjc-enigma <enigma@pku.edu.cn>
2016-12-12 02:08:23 +0800

    local merged

--- two_sigma/bin/demo.py
+++ two_sigma/bin/demo.py
@@ -1,1 +1,12 @@
+import numpy as np
+import h5py
+
 data_file = "../data/train.h5"
+
+
+hf = h5py.File(data_file, 'r') 
+print("keys in data:", list(hf.keys()))
+train_data = hf.get('train')
+np_data = np.array(train_data)
+print("data.shape", np_data.shape)
+
