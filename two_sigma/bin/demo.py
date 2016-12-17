6ce58223ad9b6ec4009ca8b37b225f9f7a8aede2
zjc-enigma <enigma@pku.edu.cn>
2016-12-17 23:36:38 +0800

    12:00

--- two_sigma/bin/demo.py
+++ two_sigma/bin/demo.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import h5py
 
 data_file = "../data/train.h5"
@@ -7,6 +8,14 @@ data_file = "../data/train.h5"
 hf = h5py.File(data_file, 'r') 
 print("keys in data:", list(hf.keys()))
 train_data = hf.get('train')
+column_name = list(train_data.keys())
+axis0 = train_data.get('axis0')
+axis1 = train_data.get('axis1')
+block0_items = train_data.get('block0_items')
+block0_values = train_data.get('block0_values')
+block1_items = train_data.get('block1_items')
+block1_values = train_data.get('block1_values')
 np_data = np.array(train_data)
 print("data.shape", np_data.shape)
 
+hdf = pd.read_hdf(data_file)
