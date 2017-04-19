008454d88f6b4b3b87e8d24f65671e0204466a70
zjc-enigma <enigma@pku.edu.cn>
2017-04-19 23:54:28 +0800

    add more param tune

--- Titanic/bin/xgb.py
+++ Titanic/bin/xgb.py
@@ -1,4 +1,5 @@
 #import tensorflow as tf
+#import multiprocessing as mp; mp.set_start_method('forkserver')
 import xgboost as xgb
 import pandas as pd
 import numpy as np
@@ -14,6 +15,8 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 import matplotlib.pylab as plt
 from sklearn.model_selection import GridSearchCV
+
+
 #from ensemble import base_train, base_test, X_train, X_test, Y_train, id_test
 import pdb
 sys.path.append('../lib')
@@ -24,10 +27,13 @@ learning_rate = 0.1
 gamma = 0.
 seed = 42
 nthread = 88
-max_depth = 5
 n_estimators = 1000
 subsample = 0.8
 colsample_bytree = 0.8
+
+# tuned
+max_depth = 3
+min_child_weight = 5
 
 # basic settings
 #target = 'Survived'
@@ -82,12 +88,69 @@ X_train = encoded[:train_rows, :]
 X_test = encoded[train_rows:, :]
 
 
+
+# tune max_depth & min_child_weight
+param_t1 = {
+    'max_depth': [3, 5, 7, 9],
+    'min_child_weight': [1, 3, 5]
+}
+
+#        seed=seed,
+gs1 = GridSearchCV(
+    estimator=xgb.XGBClassifier(
+        learning_rate=learning_rate,
+        n_estimators=150,
+        gamma=0,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        seed=seed,
+        nthread=88),
+    param_grid=param_t1,
+    scoring='roc_auc',
+    iid=False,
+    cv=5)
+
+gs1.fit(X_train.todense(), Y_train.Survived)
+print(gs1.grid_scores_)
+print(gs1.best_params_)
+print(gs1.best_score_)
+
+# tune parameters more accuracy
+# {'min_child_weight': 1, 'max_depth': 3}
+param_t2 = {
+    'max_depth': [2,3,4],
+    'min_child_weight': [1, 2]
+}
+
+gs2 = GridSearchCV(
+    estimator=xgb.XGBClassifier(
+        learning_rate=learning_rate,
+        n_estimators=150,
+        gamma=0,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        seed=seed,
+        nthread=88),
+    param_grid=param_t2,
+    scoring='roc_auc',
+    iid=False,
+    cv=5)
+
+gs2.fit(X_train.todense(), Y_train.Survived)
+print(gs2.grid_scores_)
+print(gs2.best_params_)
+print(gs2.best_score_)
+
+
+
+
 model = xgb.XGBClassifier(
     learning_rate=learning_rate,
     nthread=nthread,
     gamma=gamma,
     seed=seed,
     max_depth=max_depth,
+    min_child_weight
     n_estimators=n_estimators,
     subsample=subsample,
     colsample_bytree=colsample_bytree
@@ -100,42 +163,11 @@ modelfit(model, X_train, Y_train.Survived)
 
 model.fit(X_train, Y_train.Survived)
 
-
 pred = model.predict(X_test)
 result_list = [round(value) for value in pred]
 
 
 result_df = pd.concat([id_test, pd.DataFrame(result_list)], axis=1)
 result_df.columns = ["PassengerId" , "Survived"]
-#result_df['Survived'] = result_df.Survived.apply(int)
+result_df['Survived'] = result_df.Survived.apply(int)
 result_df.to_csv('../data/xgb_result_to_submission', index=False)
-
-
-# tune max_depth & min_child_weight
-
-param_t1 = {
-    'max_depth': [3, 5, 7, 9],
-    'min_child_weight': [1, 3, 5]
-}
-
-#        seed=seed,
-gs1 = GridSearchCV(
-    estimator=xgb.XGBClassifier(
-        learning_rate=learning_rate,
-        n_estimators=150,
-        max_depth=5,
-        min_child_weight=1,
-        gamma=0,
-        subsample=0.8,
-        colsample_bytree=0.8,
-        nthread=22),
-    param_grid=param_t1,
-    scoring='roc_auc',
-    n_jobs=4,
-    iid=False,
-    cv=5)
-
-gs1.fit(X_train.todense(), Y_train.Survived)
-gs1.grid_scores_
-gs1.best_params_
-gs1.best_score_
